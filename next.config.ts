9ca619aaee8bf5383ca47ede5dc4f90bd32dccf1
Rahul Raj <rrajchaudhary.hajipur@gmail.com>
2025-06-12 18:26:33 +0530

    First Look of website

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
+
+
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images5.alphacoders.com",
+        pathname: "/941/**",
+      },
+    ],
+  },  
+  experimental: {
+    serverActions: {},
+  },
 };
 
 export default nextConfig;
